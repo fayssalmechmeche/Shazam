af716e8e932447ed4c94d76a86addaa922136660
Flora <flora.julie88@gmail.com>
2023-01-05 15:06:32 +0100

    perso

--- shazam/index.js
+++ shazam/index.js
@@ -3,7 +3,11 @@
  */
 
 import {AppRegistry} from 'react-native';
+<<<<<<< refs/remotes/origin/main
 import App from './src/screen/login_register/register/Register.js';
+=======
+import App from './src/screen/musicList/musicList';
+>>>>>>> perso
 import {name as appName} from './app.json';
 
 // './src/App'
--- shazam/src/screen/login_register/logOrRegister.js
+++ shazam/src/screen/login_register/logOrRegister.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ButtonShazam from './components/Button';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   SafeAreaView,
   View,
